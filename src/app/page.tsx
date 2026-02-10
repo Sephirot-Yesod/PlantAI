46cfaf1f3e9f5fb762e5b81a39b5e140c1798daa
Max <max.yesod@gmail.com>
2026-02-10 09:50:33 +0800

    removed CES

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import Navigation from "@/components/Navigation";
-import CESLanding from "@/components/CESLanding";
 import Hero from "@/components/Hero";
 import ProductsShowcase from "@/components/ProductsShowcase";
 import About from "@/components/About";
@@ -10,7 +9,6 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-[#0a0a0a]">
       <Navigation />
-      <CESLanding />
       <Hero />
       <ProductsShowcase />
       <About />
